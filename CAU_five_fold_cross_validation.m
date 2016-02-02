2eb07cac9356f03f72d494cd32de893045f6a0e9
xiaohusun <xiaohusun@pku.edu.cn>
2016-02-02 15:36:51 +0800

    ICIP2016_CAU_5_fold_cross_validation
    ICIP2016_CAU_5_fold_cross_validation

--- CAU_five_fold_cross_validation.m
+++ CAU_five_fold_cross_validation.m
@@ -1,10 +1,13 @@
 load('rand.mat'); 
 % rand.mat generated using randperm contains a random permutation of the
 % integers from 1 to 5493 inclusive.
-for f=1:5,
+for fold=1:5,
     % index of training samples and test samples of each fold
-    index_test=rand(f:5:end); 
-	
+    % 1 corresponds to the first image name in the CAU_image_name_list.dat
+    % 2 corresponds to the second image name in the CAU_image_name_list.dat
+    % ...
     index_train=rand;
-    index_train(f:5:end)=[];
+    index_train(fold:5:end)=[];
+    
+    index_test=rand(fold:5:end); 
 end
